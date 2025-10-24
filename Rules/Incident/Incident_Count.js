8c327aee0ac0fffba7b95a50562cc8e5fbe0908e
JITENDRA KANSAL <jitendra.kansal@sap.com>
2025-10-24 21:39:02 +0530

    updated

--- Rules/Incident/Incident_Count.js
+++ Rules/Incident/Incident_Count.js
@@ -1,5 +1,5 @@
 export default function IncidentCount(context) {
 	return context.count('/MDK_MCP_Demo/Services/IncidentManagement.service', 'Incident', ``).then((count) => {
-        return `Incidents (${count})`;
+     return `Incidents (${count})`;
     });
 }
